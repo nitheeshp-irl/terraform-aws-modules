c185d7c21998b4bc1aee09496d7f1f41d1e2c673
nitheeshp-irl <148543591+nitheeshp-irl@users.noreply.github.com>
2024-06-30 22:55:25 +0100

    debugging

--- aws_org/main.tf
+++ aws_org/main.tf
@@ -6,25 +6,28 @@ data "aws_organizations_organization" "org" {
   # This data source does not require any arguments
 }
 
-# Create a map of parent OUs by their names
 locals {
   root_id = data.aws_organizations_organization.org.roots[0].id
 }
 
-# Create a separate data source for each parent OU to get their IDs
-data "aws_organizations_organizational_unit" "parents" {
-  for_each = toset([for ou in var.organizational_units : ou.parent_name])
-  name     = each.value
+resource "aws_organizations_organizational_unit" "parent" {
+  for_each = {
+    for ou in var.organizational_units : ou.parent_name => ou if ou.parent_name != "root"
+  }
+  name     = each.key
   parent_id = local.root_id
 }
 
 locals {
-  parent_ids = { for name, parent in data.aws_organizations_organizational_unit.parents : name => parent.id }
+  parent_ids = merge(
+    { root = local.root_id },
+    { for k, v in aws_organizations_organizational_unit.parent : k => v.id }
+  )
 
   organizational_units = [
     for ou in var.organizational_units : {
       unit_name = ou.unit_name
-      parent_id = lookup(local.parent_ids, ou.parent_name, local.root_id)
+      parent_id = local.parent_ids[ou.parent_name]
     }
   ]
 }
