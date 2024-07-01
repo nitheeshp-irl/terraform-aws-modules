715df729fa2104f7710d29b99fd725487c537e13
nitheeshp-irl <148543591+nitheeshp-irl@users.noreply.github.com>
2024-07-01 21:16:16 +0100

    debugging code

--- aws_org/main.tf
+++ aws_org/main.tf
@@ -1,3 +1,16 @@
+variable "aws_region" {
+  description = "The AWS region to create resources in"
+  type        = string
+}
+
+variable "organizational_units" {
+  description = "List of organizational units"
+  type = list(object({
+    unit_name = string
+    parent_id = string
+  }))
+}
+
 provider "aws" {
   region = var.aws_region
 }
